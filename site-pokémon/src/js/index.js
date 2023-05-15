438522544aa7ab60e9b817ae571993a1efec8e46
João Campos <joaon.c.jv@gmail.com>
2023-05-14 21:01:25 -0300

    add js

---
+++ site-pokémon/src/js/index.js
@@ -0,0 +1,19 @@
+botaoAlterarTema = document.getElementById("botao-alterar-tema");
+const body = document.querySelector("body");
+const imagemBotaoTrocaDeTema = document.querySelector(".botao");
+
+botaoAlterarTema.addEventListener("click", () => {
+    const modoEscuroAtivo = body.classList.contains("modo-escuro");
+
+    if (modoEscuroAtivo){
+        body.classList.remove("modo-escuro");
+
+        imagemBotaoTrocaDeTema.setAttribute("src", "../src/imagens/sun.png");
+    }
+
+    else{
+        body.classList.add("modo-escuro");
+        imagemBotaoTrocaDeTema.setAttribute("src","../src/imagens/moon.png")
+    }
+    
+});
